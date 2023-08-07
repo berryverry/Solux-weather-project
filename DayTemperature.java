50e2ba73413dd12628054d45c7a8685cb3940aa2
roddms <122669430+roddms@users.noreply.github.com>
2023-08-07 19:49:49 +0900

    Add files via upload

--- DayTemperature.java
+++ DayTemperature.java
@@ -22,8 +22,11 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Random;
 
 //초단기예보
 @Controller
@@ -36,7 +39,7 @@ public class DayTemperature {
     private String ny = "126"; // y좌표
     private String baseDate = strToday; // 조회하고 싶은 날짜
     private LocalDateTime now = LocalDateTime.now(); // 현재 시각을 조회 시각에.
-    int hour = now.getHour();
+    int hour = now.getHour()-1;
     private String baseTime = hour + "00";
     private String fTime = hour+1 + "00";
     private String type = "xml"; // 조회하고 싶은 type
@@ -87,6 +90,7 @@ public class DayTemperature {
             // XML에서 필요한 데이터 추출
             NodeList itemList = document.getElementsByTagName("item");
             StringBuilder weatherInfo = new StringBuilder();
+            StringBuilder ootdInfo = new StringBuilder();
             StringBuilder skyInfo = new StringBuilder();
 
             double[] TemArray = new double[10];
@@ -115,21 +119,71 @@ public class DayTemperature {
                     weatherInfo.append(fcstTime).append("시: ").append(fcstValue).append("℃\n");
 
                 }
-            }
 
-            /*int[] tems = new int[6];
-            for(int j=0; j<6; j++) {
-                tems[j] = Integer.parseInt(fcstValue);
             }
-            int max = -1000;
-            for(int x=0; x<6; x++) {
-                if(max<tems[x]) max = tems[x];
-            }
-            weatherInfo.append("최고기온은: ").append(max);*/
+            List<String> tops = Arrays.asList("프린팅 반팔", "니트 슬리브리스", "고슬고슬 니트", "반팔셔츠");
+            List<String> outers = Arrays.asList("-", "여름용 가디건", "얇은 후드집업");
+            List<String> bottoms = Arrays.asList("린넨팬츠", "코튼팬츠", "나일론 카고팬츠", "데님 반바지");
+            List<String> points = Arrays.asList("곱창머리끈", "사파리햇", "-", "비즈 팔찌");
 
+            Random random = new Random();
+            int randomTopIndex = random.nextInt(tops.size());
+            int randomOuterIndex = random.nextInt(outers.size());
+            int randomBottomIndex = random.nextInt(bottoms.size());
+            int randomPointIndex = random.nextInt(points.size());
+
+            String top = tops.get(randomTopIndex);
+            String outer = outers.get(randomOuterIndex);
+            String bottom = bottoms.get(randomBottomIndex);
+            String point = points.get(randomPointIndex);
+
+            for (int i = 0; i < itemList.getLength(); i++) {
+                Element item = (Element) itemList.item(i);
+                Object category = item.getElementsByTagName("category").item(0).getTextContent();
+                String fcstValue = item.getElementsByTagName("fcstValue").item(0).getTextContent();String baseDate = item.getElementsByTagName("baseDate").item(0).getTextContent();
+                if(category.equals("T1H")){
+                    double temperatureValue = Double.parseDouble(fcstValue);
+                    if (temperatureValue >= 28) {
+                        model.addAttribute("outer", outer);
+                        model.addAttribute("top", top);
+                        model.addAttribute("bottom", bottom);
+                        model.addAttribute("point", point);
+                    } else if (temperatureValue >= 23) {
+                        model.addAttribute("top", "반팔");
+                        model.addAttribute("outer", "가디건");
+                        model.addAttribute("bottom", "반바지, 얇은 나일론 소재의 긴바지");
+                    } else if (temperatureValue >= 20) {
+                        model.addAttribute("top", "긴팔 셔츠,반팔 위 긴 셔츠나, 반팔과 핸드워머");
+                        model.addAttribute("outer", "-");
+                        model.addAttribute("bottom", "긴바지");
+                    } else if (temperatureValue >= 17) {
+                        model.addAttribute("top", "긴팔");
+                        model.addAttribute("outer", "0온스 학잠, 반팔 위 겉옷 ");
+                        model.addAttribute("bottom", "긴바지");
+                    } else if (temperatureValue >= 12) {
+                        model.addAttribute("top", "니트,블레이저");
+                        model.addAttribute("outer", "2온스 학잠, 가디건, 후드집업");
+                        model.addAttribute("bottom", "-");
+                    } else if (temperatureValue >= 9) {
+                        model.addAttribute("top", "보온을 위한 목티, 히트텍 ,기모 후드티");
+                        model.addAttribute("outer", "기모 후드집업");
+                        model.addAttribute("bottom", "-");
+                    } else if (temperatureValue >= 5) {
+                        model.addAttribute("top", "보온을 위한 목티, 히트텍");
+                        model.addAttribute("outer", "코트, 가죽자켓, 플리스");
+                        model.addAttribute("bottom", "반바지, 얇은 나일론 소재의 긴바지");
+                    } else {
+                        model.addAttribute("top", "보온을 위한 목티, 히트텍");
+                        model.addAttribute("outer", "패딩, 코트, 목도리");
+                        model.addAttribute("bottom", "-");
+                    }
+                    model.addAttribute("tem", temperatureValue);
+                }
+            }
             model.addAttribute("weatherInfo", weatherInfo);
             model.addAttribute("skyInfo", skyInfo);
-            return "day";
+            model.addAttribute("ootdInfo", ootdInfo);
+            return "ootd";
         } catch (Exception e) {
             e.printStackTrace();
             model.addAttribute("error", "Error occurred while fetching weather data.");
